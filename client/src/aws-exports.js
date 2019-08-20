c447df487ed2eb7b3631383c6e81cbe63c3128b2
Farmin Farzin <f.farzin@digitalattitude.com>
2019-08-20 18:19:41 +0200

    :hammer: add aws exports

---
+++ client/src/aws-exports.js
@@ -0,0 +1,11 @@
+// @flow
+
+const awsExports = {
+  Auth: {
+    userPoolId: "eu-west-1_ATHvYqbn6",
+    userPoolWebClientId: "1do9qlttts6q9c5r188go6iq51",
+    region: "eu-west-1",
+  },
+};
+
+export default awsExports;
