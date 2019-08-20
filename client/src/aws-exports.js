46d40daee703b54cd9dfffabf67ef2be7754ca3f
Farmin Farzin <f.farzin@digitalattitude.com>
2019-08-20 22:28:28 +0200

    :fire: aws export

--- client/src/aws-exports.js
+++ client/src/aws-exports.js
@@ -2,8 +2,8 @@
 
 const awsExports = {
   Auth: {
-    userPoolId: "eu-west-1_ATHvYqbn6",
-    userPoolWebClientId: "1do9qlttts6q9c5r188go6iq51",
+    userPoolId: "xxx",
+    userPoolWebClientId: "xxx",
     region: "eu-west-1",
   },
 };
