9069a8c7a4d0e78a95eb83e41bbbcd4ed4b350f0
Farmin Farzin <f.farzin@digitalattitude.com>
2019-08-26 15:34:07 +0200

    :recycle: addin post confirmation for sample

--- sample/index.ts
+++ sample/index.ts
@@ -1,14 +1,21 @@
-import { Stack, StackProps, Construct, App } from "@aws-cdk/core";
+import { Stack, StackProps, Construct, App, Duration } from "@aws-cdk/core";
 import { StringParameter } from "@aws-cdk/aws-ssm";
 import { CdkPasswordless } from "../lib/index";
+import { Function, Runtime, Code } from "@aws-cdk/aws-lambda";
 
 class myStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
+    const postConfirmation = new Function(this, "postConfirmation", {
+      runtime: Runtime.NODEJS_10_X,
+      code: Code.fromAsset("./functions"),
+      handler: "postConfirm.handler"
+    });
     const pless = new CdkPasswordless(this, "myPasswordLess", {
       mailSubject: "myStack - signIn",
       userPoolClientName: "myClientName",
-      verifiedDomains: ["gmail.com"]
+      verifiedDomains: ["gmail.com"],
+      postConfirmationLambda: postConfirmation
     });
 
     new StringParameter(this, "userPoolIdParam", {
